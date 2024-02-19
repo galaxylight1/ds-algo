e3d0f4a107e9462aa558a4c7a204657e127b72c0
prateek <prateeksingh8841@gmail.com>
2024-02-19 15:57:57 -0700

    feat: ds

--- ds.java
+++ ds.java
@@ -4,7 +4,7 @@ public class ds {
     static Scanner scn = new Scanner(System.in);
 
     public static void main(String[] args) {
-        benjaminBulbs(args);
+        spanOfArray(args);
     }
 
     public static void primeOrNonPrime(String[] args) {
@@ -240,6 +240,24 @@ public class ds {
     }
 
     public static void spanOfArray(String[] args) {
-        
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        int max = arr[0];
+        int min = arr[0];
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+            if(arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println(max - min);
     }
 }
