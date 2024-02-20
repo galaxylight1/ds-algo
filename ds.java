bc95f7b482446519b1172f1884b816d16a05741a
prateek <prateeksingh8841@gmail.com>
2024-02-19 17:54:46 -0700

    feat: ds

--- ds.java
+++ ds.java
@@ -4,7 +4,7 @@ public class ds {
     static Scanner scn = new Scanner(System.in);
 
     public static void main(String[] args) {
-        spanOfArray(args);
+        barChart(args);
     }
 
     public static void primeOrNonPrime(String[] args) {
@@ -259,5 +259,30 @@ public class ds {
         }
 
         System.out.println(max - min);
+    }
+
+    public static void barChart(String[] args) {
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < arr.length; i++) arr[i] = scn.nextInt();
+
+        int max = arr[0];
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        for(int i = max; i >= 1; i--) {
+            for(int j = 0; j < arr.length; j++) {
+                if(arr[j] >= i) {
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
