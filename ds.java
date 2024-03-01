783d4864f068976dc55d622950a099613c74acd3
prateek <prateeksingh8841@gmail.com>
2024-03-01 14:30:20 -0700

    feat: ds

--- ds.java
+++ ds.java
@@ -4,7 +4,7 @@ public class ds {
     static Scanner scn = new Scanner(System.in);
 
     public static void main(String[] args) {
-        2dArrays(args);
+        matrixMultiplication(args);
     }
 
     public static void primeOrNonPrime(String[] args) {
@@ -872,6 +872,38 @@ public class ds {
 
         for(int i = 0; i < arr.length; i++) {
             for(int j = 0; j < arr[i].length; j++) System.out.println(arr[i][j]);
+        }
+    }
+
+    public static void matrixMultiplication(String[] args) {
+        int n1 = scn.nextInt();
+        int m1 = scn.nextInt();
+        int[][] one = new int[n1][m1];
+        for(int i = 0; i < n1; i++) {
+            for(int j = 0; j < one[i].length; j++) one[i][j] = scn.nextInt();
+        }
+
+        int n2 = scn.nextInt();
+        int m2 = scn.nextInt();
+        int[][] two = new int[n2][m2];
+        for(int i = 0; i < n2; i++) {
+            for(int j = 0; j < two[i].length; j++) two[i][j] = scn.nextInt();
+        }
+
+        if(m1 != n2) return;
+
+        int[][] res = new int[n1][m2];
+        for(int i = 0; i < res.length; i++) {
+            for(int j = 0; j < res[i].length; j++) {
+                for(int k = 0; k < m1; k++) {
+                    res[i][j] += one[i][k] * two[k][j];
+                }
+            }
+        }
+        
+        for(int i = 0; i < res.length; i++) {
+            int[] temp = res[i];
+            System.out.println(Arrays.toString(temp));
         }
     }
 }
