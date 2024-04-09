a14237f61afcb7e299010390630d27cc832ef5e4
prateek <prateeksingh8841@gmail.com>
2024-04-09 01:17:51 -0600

    🎲 feat: ds

--- Queues/Queues.js
+++ Queues/Queues.js
@@ -10,6 +10,25 @@ class Queue {
     remove() {
         return this.data.pop();
     }
+
+    peek() {
+        return this.data[this.data.length - 1];
+    }
+}
+
+function weave(sourceOne, sourceTwo) {
+    const q = new Queue();
+
+    while(sourceOne.peek() || sourceTwo.peek()) {
+        if(sourceOne.peek()) {
+            q.add(sourceOne.remove());
+        }
+        if(sourceTwo.peek()) {
+            q.add(sourceTwo.remove());
+        }
+    }
+
+    return q;
 }
 
 module.exports = Queue;
